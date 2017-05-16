16a0a4616bb5eac1acb23f0f402239d662293ebf
Jacqlyn C Weir <weirjacqlyn89@gmail.com>
2017-05-15 22:53:20 -0700

    Add local storage

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ $("#add-new-book").submit(function(e) {
 
   $(formData).each(function(index, item) {
 
-    if (item.name === "details[][title]") {
+    if (item.name === "details[][title]" && item.value != "") {
 
       // Next item is always the value
       var detailsValue = formData[index + 1];
@@ -35,7 +35,16 @@ $("#add-new-book").submit(function(e) {
     }
   });
 
-  window.books.push(book);
+  // Get current library state
+  var library = JSON.parse(localStorage.getItem("library"));
+
+  library.push(book);
+
+  // Update local storage
+  localStorage.setItem("library", JSON.stringify(library));
+
+  // Update the collection view
+  renderCollectionView(library);
 
   // Clear add book form
   $("#add-new-book input, #add-new-book textarea")
@@ -47,37 +56,42 @@ $("#add-new-book").submit(function(e) {
   $(".add-form-details-container")
     .empty()
     .append(detailsItemInput);
-
-  // Update the collection view
-  renderCollectionView();
 });
 
+function deleteBookHandler(bookId) {
 
-function renderCollectionViewItem(bookId, item) {
-  return $("<div>")
-    .attr("id", "book-" + bookId)
-    .addClass("col-lg-3 col-md-4 col-sm-6 col-xs-12 book")
-    .append($("<a>")
-      .addClass("book-item-container")
-      .append($("<img>")
-        .addClass("portfolio-item")
-        .attr("src", item.image)
-        .attr("alt", "Book Cover Image")
-      )
-      .append($("<h4>").text(item.title))
-    )
-    .click(function() {
-      renderDetailViewItem(bookId, item);
-    });
+  // Get the current library
+  var library = JSON.parse(localStorage.getItem("library"));
+
+  // Remove the deleted book
+  library = library.filter(function(item, index) {
+    return index != bookId;
+  });
+
+  // Update the library
+  localStorage.setItem("library", JSON.stringify(library));
+
+  // Re-render the page
+  renderPage();
 }
 
-function renderDetailViewItem(bookId, item) {
+function renderDetailViewItem(bookId, book) {
 
   // Scroll to the top of the page
   $("body").scrollTop(0);
 
+  // Remove active class from old book
   $(".book").removeClass("active");
 
+  // No book found, let them know
+  if (!book) {
+    $("#book-detail-container")
+      .empty()
+      .text("No Book Selected");
+    return;
+  }
+
+  // Apply active class to selected book
   $("#book-" + bookId).addClass("active");
 
   // Render details title section
@@ -87,8 +101,12 @@ function renderDetailViewItem(bookId, item) {
       .addClass("col-lg-12")
       .append($("<h1>")
         .addClass("page-header")
-        .text(item.title)
-        .append($("<small>").text(item.author))
+        .text(book.title)
+        .append($("<small>").text(book.author))
+        .append($("<button>")
+        .addClass("btn btn-danger pull-right")
+        .text("Delete")
+        .click(deleteBookHandler.bind(null, bookId)))
       ));
 
   // Render details image section
@@ -96,22 +114,23 @@ function renderDetailViewItem(bookId, item) {
     .addClass("col-md-4")
     .append($("<img>")
       .addClass("img-responsive cover-image")
-      .attr("src", item.image)
-      .attr("alt", item.title + " cover"));
+      .attr("src", book.image)
+      .attr("alt", book.title + " cover"));
 
+  // Render book details
   var detailsList = $("<ul>");
-
-  $(item.details).each(function(index, item) {
+  $(book.details).each(function(index, item) {
     detailsList.append($("<li>")
       .addClass("detail-list-item")
       .append($("<strong>").text(item.label + ":"))
       .append($("<span>").text(item.value)));
   });
 
+  // Render book description
   var detailsDescription = $("<div>")
     .addClass("col-md-8")
     .append($("<h3>").text("Description"))
-    .append($("<p>").text(item.description))
+    .append($("<p>").text(book.description))
     .append($("<h3>").text("Details"))
     .append(detailsList);
 
@@ -124,16 +143,39 @@ function renderDetailViewItem(bookId, item) {
       .append(detailsDescription));
 }
 
-function renderCollectionView() {
-  $("#related-books-container").empty();
+function renderCollectionViewItem(bookId, book) {
+  return $("<div>")
+    .attr("id", "book-" + bookId)
+    .addClass("col-lg-3 col-md-4 col-sm-6 col-xs-12 book")
+    .append($("<a>")
+      .addClass("book-item-container")
+      .append($("<img>")
+        .addClass("portfolio-item")
+        .attr("src", book.image)
+        .attr("alt", "Book Cover Image")
+      )
+      .append($("<h4>").text(book.title))
+    )
+    .click(renderDetailViewItem.bind(null, bookId, book));
+}
 
-  $.each(window.books, function(key, item) {
+function renderCollectionView(library) {
+  $("#related-books-container").empty();
+  $.each(library, function(bookId, book) {
     $("#related-books-container")
-      .append(renderCollectionViewItem(key, item));
+      .append(renderCollectionViewItem(bookId, book));
   });
 }
 
-renderCollectionView();
+function renderPage() {
+  // Get all books in library
+  var library = JSON.parse(localStorage.getItem("library"));
 
-// Show first book in the list
-renderDetailViewItem(0, books[0]);
+  // Render list of books
+  renderCollectionView(library);
+
+  // Render details for the first book
+  renderDetailViewItem(0, library[0]);
+}
+
+renderPage();
